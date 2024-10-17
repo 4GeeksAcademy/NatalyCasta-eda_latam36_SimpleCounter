7bcae50a4a197b027d81aaf3d268ec52fc458a24
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-17 05:22:36 +0000

    added counter functionality with setInterval re-render

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -4,17 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-regular-svg-icons'
 
 const Counter = (props) => {
+  const paddedSeconds = String(props.seconds).padStart(6, "0");
+  const digits = paddedSeconds.split("");
   return (
     <div className='containerBoxes'>
       <div className='containerClock'>
-      <div><FontAwesomeIcon icon={faClock} /></div>
+        <div><FontAwesomeIcon icon={faClock} /></div>
       </div>
-      <div className='containerNumbers'>0</div>
-      <div className='containerNumbers'>0</div>
-      <div className='containerNumbers'>0</div>
-      <div className='containerNumbers'>0</div>
-      <div className='containerNumbers'>0</div>
-      <div className='containerNumbers'>0</div>
+      <div className='containerNumbers'>{digits[0]}</div>
+      <div className='containerNumbers'>{digits[1]}</div>
+      <div className='containerNumbers'>{digits[2]}</div>
+      <div className='containerNumbers'>{digits[3]}</div>
+      <div className='containerNumbers'>{digits[4]}</div>
+      <div className='containerNumbers'>{digits[5]}</div>
     </div>
   )
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Counter from "./Counter.jsx"
 //create your first component
 const Home = () => {
+	let seconds = 0
+	setInterval(() => {
+		console.log(seconds)
+		seconds++
+	}, 1000)
 	return (
 		<div className="text-center">
-		
-			<Counter></Counter>
+
+			<Counter>
+
+			</Counter>
 		</div>
-		
+
 	);
+
 };
 
 export default Home;
